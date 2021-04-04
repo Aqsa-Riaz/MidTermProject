178987ce855d548921310e624c9c2e90e0f60eb2
Aqsa-Riaz <aqsariaz114@gmail.com>
2021-04-04 12:47:01 +0500

    Midterm Project

--- MidTermProject/src/midtermproject/MidTermProject.java
+++ MidTermProject/src/midtermproject/MidTermProject.java
@@ -16,6 +16,8 @@ public class MidTermProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Form1 F1=new Form1();
+        
     }
     
 }
